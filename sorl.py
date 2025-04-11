9450931ec225cbece7e0a7450771328c42268acb
thegreenn1nja <73561889+thegreenn1nja@users.noreply.github.com>
2025-04-11 13:22:40 -0500

    Sorl is the good one
    changed functions and did some stuff to try and understand

--- sorl.py
+++ sorl.py
@@ -7,20 +7,17 @@ import numpy
 from deap import algorithms, base, creator, tools, gp
 
 
-# Define new functions
-def SmallOrLarge(x):
-    if x >= 1000 & x < 2000:
-        return x
-    elif x >= 2000:
-        return 1
-    else:
-        return 0 # We can change the 0 and 1 to Small and Large to fit with the problem
 
-def IntToString(x):
-    if x == 0:
-        return "Small"
-    elif x == 1:
-        return "Large"
-    else:
-        return "Mid" 
-    
+# Define new functions
+
+def less_than(a: int, b: int) -> bool:
+    return a < b
+
+def greater_or_equal(a: int, b: int) -> bool:
+    return a >= b
+
+def greater_than(a: int, b: int) -> bool:
+    return a > b
+
+
+pset = gp.PrimitiveSet("MAIN", [int], str)
