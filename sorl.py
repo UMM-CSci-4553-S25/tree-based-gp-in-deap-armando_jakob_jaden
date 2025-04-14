f55f4a89a7e51b966f764d237ffbe2e2e2b1c892
thegreenn1nja <73561889+thegreenn1nja@users.noreply.github.com>
2025-04-14 15:20:56 -0500

    IT LKLINDASNDkasn

--- sorl.py
+++ sorl.py
@@ -16,13 +16,25 @@ def less_than(x: int, y: int) -> bool:
 def equals(x: int, y: int) -> bool:
     return x == y
 
-def label_large(x: int) -> str:
-    # print("large")
-    return "large"
+def less_than_or_equal_to(x: int, y: int) -> bool:
+    return x <= y
 
-def label_small(x: int) -> str:
-    # print("small")
-    return "small"
+def greater_than_or_equal_to(x: int, y: int) -> bool:
+    return x >= y
+
+# def label_large(x: int) -> str:
+#     # print("large")
+#     if x >= 2000:
+#         return "large"
+#     elif 1000 <= x < 2000:
+#         return "none"
+
+# def label_small(x: int) -> str:
+#     # print("small")
+#     if x < 1000:
+#         return "small"
+#     elif 1000 <= x < 2000:
+#         return "none"
 
 def if_then_else(condition: bool, out1, out2):
     return out1 if condition else out2
@@ -35,12 +47,16 @@ pset.renameArguments(ARG0="x")
 pset.addPrimitive(greater_than, 2, name="gt")
 pset.addPrimitive(less_than, 2, name="lt")
 pset.addPrimitive(equals, 2, name="eq")
-pset.addPrimitive(label_large, 1)
-pset.addPrimitive(label_small, 1)
+pset.addPrimitive(less_than_or_equal_to, 2, name="le")
+pset.addPrimitive(greater_than_or_equal_to, 2, name="ge")
+# pset.addPrimitive(label_large, 1)
+# pset.addPrimitive(label_small, 1)
 pset.addPrimitive(if_then_else, 3)
-pset.addTerminal(1000)
+pset.addTerminal("large")
+pset.addTerminal("small")
 pset.addTerminal(2000)
-pset.addTerminal("none")  # Fallback for silent case
+pset.addTerminal(1000)
+# pset.addTerminal("none")
 
 # Define GP individual and fitness
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
@@ -55,11 +71,11 @@ toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("compile", gp.compile, pset=pset)
 
 # Evaluation function for classification behavior
+test_inputs = [random.randint(-10000, 10000) for _ in range(20)]
+
 def evalClassifier(individual):
     func = toolbox.compile(expr=individual)
     correct_behavior = 0
-
-    test_inputs = [random.randint(-10000, 10000) for _ in range(20)]
 
     for x in test_inputs:
         try:
@@ -71,10 +87,10 @@ def evalClassifier(individual):
             correct_behavior += 1
         elif x < 1000 and output == "small":
             correct_behavior += 1
-        elif 1000 <= x < 2000 and output == "none":
+        elif 1000 <= x < 2000 : 
             correct_behavior += 1
 
-    return 1.0 - (correct_behavior / 20),
+    return 1.0 - (correct_behavior / len(test_inputs)),
 
 toolbox.register("evaluate", evalClassifier)
 toolbox.register("select", tools.selTournament, tournsize=3)
@@ -96,7 +112,7 @@ def main():
     mstats.register("min", numpy.min)
     mstats.register("max", numpy.max)
 
-    pop, log = algorithms.eaSimple(pop, toolbox, 0.5, 0.1, 500, stats=mstats,
+    pop, log = algorithms.eaSimple(pop, toolbox, 0.5, 0.1, 100, stats=mstats,
                                    halloffame=hof, verbose=True)
 
     for winner in hof:
